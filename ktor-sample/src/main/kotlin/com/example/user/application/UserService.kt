801319e12e0d0deca4b1658bceee950f23049504
choi seoin <inseo@uupfi.com>
2023-03-23 01:09:51 +0900

    refactor: UserService to interface, create UserServiceImpl

--- ktor-sample/src/main/kotlin/com/example/user/application/UserService.kt
+++ ktor-sample/src/main/kotlin/com/example/user/application/UserService.kt
@@ -3,9 +3,16 @@ package com.example.user.application
 import com.example.user.infra.UserRepository
 import com.example.user.domain.User
 
-class UserService(private val userRepository: UserRepository) {
-    suspend fun create(user: User): Int = userRepository.create(user)
-    suspend fun read(id: Int): User? = userRepository.read(id)
-    suspend fun update(id: Int, user: User) = userRepository.update(id, user)
-    suspend fun delete(id: Int) = userRepository.delete(id)
+interface UserService {
+    suspend fun create(user: User): Int
+    suspend fun read(id: Int): User?
+    suspend fun update(id: Int, user: User)
+    suspend fun delete(id: Int)
+}
+
+class UserServiceImpl(private val userRepository: UserRepository) : UserService {
+    override suspend fun create(user: User): Int = userRepository.create(user)
+    override suspend fun read(id: Int): User? = userRepository.read(id)
+    override suspend fun update(id: Int, user: User) = userRepository.update(id, user)
+    override suspend fun delete(id: Int) = userRepository.delete(id)
 }
